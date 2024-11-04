46913f83fe44593e5bdd33f29088bd587d56488e
jonas <jonasgabriel9803@gmail.com>
2024-11-04 18:48:51 -0300

    :sparkles: feat:building the game and gameManager classes in JS

---
+++ src/js/game.js
@@ -0,0 +1,7 @@
+export class Game {
+    constructor(name,publisher, releaseDate) {
+        this.name = name;
+        this.publisher = publisher;
+        this.releaseDate = releaseDate;
+    }
+}
